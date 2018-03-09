b7539329f7316b9a0cad5974a6163ca64577a19a
sivaprakashS <31438994+sivaprakashS@users.noreply.github.com>
2018-03-09 15:46:11 +0530

    stringnum.c

---
+++ stringnum.c
@@ -0,0 +1,15 @@
+#inculde<stdeio.h>
+void main()
+{
+def firstk():
+	str=input()
+	s=''
+	k=int(input())
+	for i in range(0,k):
+		s+=str[i]
+	print(s)
+try:
+	firstk()
+except:
+	print('invalid)
+  }
